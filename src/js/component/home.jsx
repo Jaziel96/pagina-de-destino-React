467291affbb64f40502ad29f2144ce987c10d87c
Jaziel96 <jazcov96@hotmail.com>
2022-07-23 05:19:13 +0000

    pagina react footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -33,74 +33,80 @@ const Home = () => {
   </div>
 </nav>
 			</div>
-			<div className="container">
-			<div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+			
+			<div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src="https://picsum.photos/400/200" className="d-block w-100" alt="..." />
+      <img src="https://picsum.photos/400/200" className="d-block w-100" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://picsum.photos/400/202" className="d-block w-100" alt="..." />
+      <img src="https://picsum.photos/400/201" className="d-block w-100" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://picsum.photos/400/201" className="d-block w-100" alt="..." />
+      <img src="https://picsum.photos/400/202" className="d-block w-100" alt="..."/>
     </div>
   </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
   </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
     <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button>
-  <div>
-  <div className="container">
-  <div className="row row-cols-4">
-    <div className="col"><div className="card" style={{width: "18rem;"}}>
-  <img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div></div>
-    <div className="col"><div className="card" style={{width: "18rem;"}}>
-  <img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div></div>
-    <div className="col"><div className="card" style={{width: "18rem;"}}>
-  <img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div></div>
-    <div className="col"><div className="card" style={{width: "18rem;"}}>
-  <img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div></div>
-  </div>
 </div>
+					
+					<div className="row row-cols-4 mt-5">
+						<div className="col">
+							<div className="card" style={{width: "18rem;"}}>
+					<img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Go somewhere</a>
+					</div>
+					</div></div>
+						<div className="col">
+							<div className="card" style={{width: "18rem;"}}>
+					<img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Go somewhere</a>
+					</div>
+					</div></div>
+						<div className="col">
+							<div className="card" style={{width: "18rem;"}}>
+					<img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Go somewhere</a>
+					</div>
+					</div></div>
+						<div className="col">
+							<div className="card" style={{width: "18rem;"}}>
+					<img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Go somewhere</a>
+					</div>
+					</div></div>
+					</div>
+					
+					<div className="container-fluid">
+					<footer className="py-3 my-4 bg-dark">
+						<p className="text-center text-white">CopyRight© 2021 Company, Inc</p>
+					</footer>
+					</div>
+					
+					</div>
 
-  </div>
-</div>
-			</div>
-		</div>
+  
+
+			
+		
 	);
 };
 
