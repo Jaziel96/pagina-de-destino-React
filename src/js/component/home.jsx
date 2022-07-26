551e85631d54e248eef748fbcdc53fa654d174e9
Jaziel96 <jazcov96@hotmail.com>
2022-07-26 03:01:35 +0000

    creando componentes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,112 +1,35 @@
 import React from "react";
+import navbar from "./navbar.jsx";
+import carousele from "./carousele.jsx";
+import card from "./card.jsx";
+import footer from "./footer.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar.jsx";
+import Carousele from "./carousele.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<div>
-			<nav className="navbar navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-			</div>
+			<Navbar/>
 			
-			<div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="https://picsum.photos/400/200" className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src="https://picsum.photos/400/201" className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src="https://picsum.photos/400/202" className="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
+			<Carousele/>
+
+			<Card/>
+			
+			<Footer/>
+			
 					
-					<div className="row row-cols-4 mt-5">
-						<div className="col">
-							<div className="card" style={{width: "18rem;"}}>
-					<img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" className="btn btn-primary">Go somewhere</a>
-					</div>
-					</div></div>
-						<div className="col">
-							<div className="card" style={{width: "18rem;"}}>
-					<img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" className="btn btn-primary">Go somewhere</a>
-					</div>
-					</div></div>
-						<div className="col">
-							<div className="card" style={{width: "18rem;"}}>
-					<img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" className="btn btn-primary">Go somewhere</a>
-					</div>
-					</div></div>
-						<div className="col">
-							<div className="card" style={{width: "18rem;"}}>
-					<img src="https://picsum.photos/400/201" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" className="btn btn-primary">Go somewhere</a>
-					</div>
-					</div></div>
-					</div>
 					
-					<div className="container-fluid">
-					<footer className="py-3 my-4 bg-dark">
-						<p className="text-center text-white">CopyRight© 2021 Company, Inc</p>
-					</footer>
-					</div>
-					
-					</div>
 
   
 
 			
-		
+			</div>
 	);
 };
 
